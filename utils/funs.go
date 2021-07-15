abed652ba61f855844ac1d293053e2fa1097c475
Dmitry <pro100pro10010@gmail.com>
2021-07-15 16:26:22 +0300

    Delete GoLang

--- utils/funs.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-func Ternary(condition bool, objectIfTrue interface{}, objectIfFalse interface{}) interface{} {
-	if condition {
-		return objectIfTrue
-	} else {
-		return objectIfFalse
-	}
-}
