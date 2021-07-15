21aafe5d4aa6edd86681450d418f5dabb16e105d
Dmitry <pro100pro10010@gmail.com>
2021-07-15 17:28:54 +0300

    Start Rust

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use std::env;
+
+
+fn env_get(x: String) -> String{
+    for (key, value) in env::vars() {
+        if key == x {
+            return value;
+        }
+    }
+    return "".to_owned();
+}
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let mut address: String = "127.0.0.1:".to_owned();
+    let port: String = env_get("PORT".to_owned());
+    address.push_str(&port);
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(address)?
+    .run()
+    .await
+}
